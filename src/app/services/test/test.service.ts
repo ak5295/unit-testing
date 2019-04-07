a5da27c32f05969fa797a7fa040fd6f8f2d2a1dc
Lillian Lee <lillianlee62@gmail.com>
2019-04-07 19:24:42 -0400

    add dom

--- src/app/services/test/test.service.ts
+++ src/app/services/test/test.service.ts
@@ -6,20 +6,33 @@ export class TestService {
 
   public testMethod2Args(arg1, arg2): Observable<any> {
 
-    return from(this.sleep(5000));
+    const retVal = {
+      msg: 'pretended to sleep',
+      list: [
+        {id: 'item1', value: 'value1'},
+        {id: 'item2', value: 'value2'},
+        {id: 'item3', value: 'value3'},
+        {id: 'item4', value: 'value4'},
+        {id: 'item5', value: 'value5'}
+      ]
+    };
+
+    return from(this.sleep(5000, retVal));
   }
 
   public testMethod1Args(arg1): Observable<any> {
 
-    return from(this.sleep(5000));
+    const retVal = {};
+    return from(this.sleep(5000, retVal));
   }
 
   public testMethodNoArgs(): Observable<any> {
 
-    return from(this.sleep(5000));
+    const retVal = {};
+    return from(this.sleep(5000, retVal));
   }
 
-  private sleep(ms): Promise<string> {
-    return new Promise(resolve => setTimeout(() => resolve('completed sleeping'), ms));
+  private sleep(ms, retVal): Promise<any> {
+    return new Promise(resolve => setTimeout(() => resolve(retVal), ms));
   }
 }
--- src/app/views/componentA/componentA.component.spec.ts
+++ src/app/views/componentA/componentA.component.spec.ts
@@ -40,9 +40,15 @@ describe('ComponentAComponent no queryParams', () => {
 
 });
 
-const mockMsg = {
+const mockMsg2Args = {
   msg: 'pretended to sleep',
-  list: []
+  list: [
+    {id: 'item1', value: 'value1'},
+    {id: 'item2', value: 'value2'},
+    {id: 'item3', value: 'value3'},
+    {id: 'item4', value: 'value4'},
+    {id: 'item5', value: 'value5'}
+  ]
 };
 describe('ComponentAComponent with both id1, id2 queryParams set', () => {
   beforeEach(async(() => {
@@ -52,7 +58,7 @@ describe('ComponentAComponent with both id1, id2 queryParams set', () => {
       ['testMethod2Args']
       );
 
-    const testMethod2ArgsSpy = testService.testMethod2Args.and.returnValue(of(mockMsg));
+    testService.testMethod2Args.and.returnValue(of(mockMsg2Args));
 
     TestBed.configureTestingModule({
       imports:[RouterTestingModule.withRoutes([
@@ -81,7 +87,63 @@ describe('ComponentAComponent with both id1, id2 queryParams set', () => {
     // right after ngOninit called
     expect(compA.model.id1).toMatch('123');
     expect(compA.model.id2).toMatch('245');
-    expect(compA.model.msg).toEqual(mockMsg.msg);
-    expect(compA.model.list).toEqual(mockMsg.list);
+    expect(compA.model.msg).toEqual(mockMsg2Args.msg);
+    expect(compA.model.list).toEqual(mockMsg2Args.list);
+  });
+
+  it('should call delay service, and populate the dom msg', () => {
+    const fixture = TestBed.createComponent(ComponentAComponent);
+    const compA: ComponentAComponent = fixture.debugElement.componentInstance;
+
+    // vvv
+    fixture.detectChanges();
+
+    // the route element of the component DOM
+    const rootElem: HTMLElement = fixture.debugElement.nativeElement;
+
+    // make sure the message is present
+    const msgDiv = rootElem.querySelector('#msg-text');
+    expect(msgDiv).toBeTruthy();
+    expect(msgDiv.tagName.toLowerCase()).toEqual('div');
+    expect(msgDiv.textContent).toEqual(mockMsg2Args.msg);
+  });
+
+  it('should call delay service, and populate the dom item list', () => {
+    const fixture = TestBed.createComponent(ComponentAComponent);
+    const compA: ComponentAComponent = fixture.debugElement.componentInstance;
+
+    // vvv
+    fixture.detectChanges();
+
+    // the route element of the component DOM
+    const rootElem: HTMLElement = fixture.debugElement.nativeElement;
+
+    // make sure all the li elements are present, for the mockMsg2Args.list
+    mockMsg2Args.list.forEach(elem => {
+      const elemId = elem.id;
+      const elemValue = elem.value;
+
+      const item1Span = rootElem.querySelector(`#${elemId}`);
+      expect(item1Span).toBeTruthy();
+      expect(item1Span.tagName.toLowerCase()).toEqual('span');
+      expect(item1Span.textContent).toEqual(elemValue);
+    });
+  });
+
+  it('should call delay service, and populate the dom item list with correct number of elements', () => {
+    const fixture = TestBed.createComponent(ComponentAComponent);
+    const compA: ComponentAComponent = fixture.debugElement.componentInstance;
+
+    // vvv
+    fixture.detectChanges();
+
+    // the route element of the component DOM
+    const rootElem: HTMLElement = fixture.debugElement.nativeElement;
+
+    // make sure the list item ul has the correct number of children
+    const itemListLiChildCollection =
+      rootElem.querySelectorAll('#item-list > li');
+    const numListItemLis = itemListLiChildCollection.length;
+    expect(numListItemLis).toEqual(mockMsg2Args.list.length);
   });
 });
